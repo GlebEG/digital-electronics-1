2a78305f5d99507c8d59fa38031a7c6856d22ce6
GlebEG <124879397+GlebEG@users.noreply.github.com>
2023-04-18 11:36:19 +0200

    Update clock_enable.vhd

--- labs/09-project/project_1.srcs/sources_1/new/clock_enable.vhd
+++ labs/09-project/project_1.srcs/sources_1/new/clock_enable.vhd
@@ -8,7 +8,7 @@ library ieee;
 
 entity clock_enable is
   generic (
-    g_MAX : natural := 5 --! Number of clk pulses to generate one enable signal period
+    g_MAX : natural := 100000000 --! Number of clk pulses to generate one enable signal period
   );                       -- Note that there IS a semicolon between generic and port sections
   port (
     clk : in    std_logic; --! Main clock
